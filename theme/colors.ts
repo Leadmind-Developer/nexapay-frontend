162188af5aae5f506ab1f295e3abc25d67d1583f
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2025-12-15 11:11:49 +0100

    Create colors.ts

---
+++ theme/colors.ts
@@ -0,0 +1,17 @@
+// theme/colors.ts
+
+const colors = {
+  primary: "#39358c",
+  primaryDark: "#39358c",
+  background: "#f9fafb",
+  surfaceDark: "#111827",
+  surfaceLight: "#ffffff",
+  textLight: "#ffffff",
+  textDark: "#111827",
+  gray: "#6b7280",
+  success: "#12C060",
+  warning: "#F5A623",
+  error: "#E84E4E",
+};
+
+export default colors;
