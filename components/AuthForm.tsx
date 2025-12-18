2ab501e3d7d602fe956abadea4b6ec45e30e8147
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2025-12-18 19:51:37 +0100

    Update AuthForm.tsx

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import api from "@/lib/api";
-import OTPInput from "@/components/OTPInput";
+import OTPInput from "@/components/auth/OTPInput";
 
 interface AuthFormProps {
   mode: "login" | "register" | "forgot";
