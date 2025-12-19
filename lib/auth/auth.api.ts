b43f799d29b3f8e2c58907a84c17dce2df7d211e
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2025-12-19 19:24:11 +0100

    Update auth.api.ts

--- lib/auth/auth.api.ts
+++ lib/auth/auth.api.ts
@@ -7,39 +7,39 @@ type OptionalConfig = AxiosRequestConfig;
 
 export const AuthAPI = {
   login(payload: Payload, config?: OptionalConfig): Promise<AxiosResponse<LoginResponse>> {
-    return api.post("/auth/login", payload, config);
+    return api.post("/auth/web/login", payload, config);
   },
 
   confirmLogin(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/confirm-login", payload, config);
+    return api.post("/auth/web/confirm-login", payload, config);
   },
 
   register(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/register", payload, config);
+    return api.post("/auth/web/register", payload, config);
   },
 
   confirmRegistration(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/confirm-registration", payload, config);
+    return api.post("/auth/web/confirm-registration", payload, config);
   },
 
   resendOtp(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/resend-otp", payload, config);
+    return api.post("/auth/web/resend-otp", payload, config);
   },
 
   forgot(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/forgot", payload, config);
+    return api.post("/auth/web/forgot", payload, config);
   },
 
   reset(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/reset", payload, config);
+    return api.post("/auth/web/reset", payload, config);
   },
 
   logout(config?: OptionalConfig) {
-    return api.post("/auth/logout", undefined, config);
+    return api.post("/auth/web/logout", undefined, config);
   },
 
   verify(config?: OptionalConfig) {
-    return api.get("/auth/verify", config);
+    return api.get("/auth/web/verify", config);
   },
 
   refresh(config?: OptionalConfig) {
@@ -47,14 +47,14 @@ export const AuthAPI = {
   },
 
   toggle2FA(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/2fa/toggle", payload, config);
+    return api.post("/auth/web/2fa/toggle", payload, config);
   },
 
   requestBiometric(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/request-biometric", payload, config);
+    return api.post("/auth/web/request-biometric", payload, config);
   },
 
   verifyBiometric(payload: Payload, config?: OptionalConfig) {
-    return api.post("/auth/verify-biometric", payload, config);
+    return api.post("/auth/web/verify-biometric", payload, config);
   },
 };
