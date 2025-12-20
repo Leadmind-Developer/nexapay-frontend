c18cd7204be38f29a4281a11b333f69b760e814d
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2025-12-20 08:21:47 +0100

    Update AuthSubmit.tsx

--- components/auth/AuthSubmit.tsx
+++ components/auth/AuthSubmit.tsx
@@ -2,15 +2,17 @@ export default function AuthSubmit({
   children,
   loading,
   onClick,
+  disabled,
 }: {
   children: React.ReactNode;
   loading?: boolean;
   onClick: () => void;
+  disabled?: boolean;
 }) {
   return (
     <button
       onClick={onClick}
-      disabled={loading}
+      disabled={loading || disabled}
       className="w-full py-3 rounded-lg bg-blue-600 text-white disabled:opacity-50"
     >
       {loading ? "Please wait…" : children}
