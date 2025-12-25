52d8b59a8063bdfa90c98179f1c08c0860d0c779
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2025-12-25 16:29:00 +0100

    Create savingsBrokenEmail.js

---
+++ emails/savingsBrokenEmail.js
@@ -0,0 +1,7 @@
+export function savingsBrokenEmail(goal) {
+  return `
+  <h2>Savings Withdrawn Early ⚠️</h2>
+  <p>Your savings was broken early.</p>
+  <p>Interest has been forfeited.</p>
+  `;
+}
