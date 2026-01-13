526ee7f460197395b1fbb2465584546f183835b7
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2026-01-13 14:11:32 +0100

    Update RechartsClient.tsx

--- components/charts/RechartsClient.tsx
+++ components/charts/RechartsClient.tsx
@@ -1,56 +1,29 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import type { ComponentType } from "react";
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  LineChart,
+  Line,
+} from "recharts";
 
-/* Helper to silence Recharts typing issues */
-const dynamicChart = <T extends ComponentType<any>>(
-  loader: () => Promise<{ default: T }>
-) =>
-  dynamic(loader, {
-    ssr: false,
-  });
-
-export const ResponsiveContainer = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.ResponsiveContainer }))
-);
-
-export const PieChart = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.PieChart }))
-);
-
-export const Pie = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.Pie }))
-);
-
-export const Cell = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.Cell }))
-);
-
-export const Tooltip = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.Tooltip }))
-);
-
-export const BarChart = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.BarChart }))
-);
-
-export const Bar = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.Bar }))
-);
-
-export const XAxis = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.XAxis }))
-);
-
-export const YAxis = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.YAxis }))
-);
-
-export const LineChart = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.LineChart }))
-);
-
-export const Line = dynamicChart(
-  () => import("recharts").then(m => ({ default: m.Line }))
-);
+export {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  LineChart,
+  Line,
+};
