8d0a5bd9e0bb96de5aaa6c86a0eae8526ddf091e
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2026-01-22 18:55:29 +0100

    Update page.tsx

--- app/organizer/events/page.tsx
+++ app/organizer/events/page.tsx
@@ -54,7 +54,7 @@ export default function OrganizerEventsPage() {
       {/* ================= TOP BAR ================= */}
       <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
         <div>         
-          <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
+          <h1 className="text-3xl font-bold text-gray-900 dark:text-black">
             Organizer Dashboard
             </h1>
           <p className="text-gray-500 dark:text-black-300 mt-1">
