59f4a156dbe682a014f17213ad85d40f3d842027
Fortemedia <124510838+Fortemedia@users.noreply.github.com>
2026-01-31 18:52:41 +0100

    Create metadata.ts

---
+++ app/organizer/metadata.ts
@@ -0,0 +1,9 @@
+// app/organizer/metadata.ts
+export const metadata = {
+  title: "Nexa Events Organizer",
+  description:
+    "Manage your events with Nexa. Create, track, and optimize your events easily. Perfect for organizers looking to grow their audience and manage ticketing.",
+  keywords:
+    "Nexa Events, event organizer, ticketing, event management, dashboard, create events, Nigeria",
+  alternates: { canonical: "https://nexa.app/organizer/events" },
+};
