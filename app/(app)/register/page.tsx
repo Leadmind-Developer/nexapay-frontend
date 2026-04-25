336ed0c47dc593bb5696c277b883a57c138320aa
Wale Steve Adedoyin <124510838+Fortemedia@users.noreply.github.com>
2026-04-25 03:01:29 +0100

    Update page.tsx

--- app/(app)/register/page.tsx
+++ app/(app)/register/page.tsx
@@ -1,9 +1,10 @@
+// app/register/page.tsx
 import AuthPage from "@/components/AuthPage";
 import AuthForm from "@/components/AuthForm";
 
 export default function RegisterPage() {
   return (
-    <AuthPage plainBackground>
+    <AuthPage videoSrc="" imageSrc="">
       <AuthForm mode="register" />
     </AuthPage>
   );
