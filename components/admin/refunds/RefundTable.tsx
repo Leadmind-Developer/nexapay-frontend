fef95fecd627fb65a93c07677751c3dea9006753
Wale Steve Adedoyin <124510838+Fortemedia@users.noreply.github.com>
2026-06-26 13:55:27 +0100

    Update RefundTable.tsx

--- components/admin/refunds/RefundTable.tsx
+++ components/admin/refunds/RefundTable.tsx
@@ -37,7 +37,7 @@ export default function RefundTable({
 
             <td>
               ₦
-              {(refund.amount.toLocaleString()}
+              {refund.amount.toLocaleString()}
             </td>
 
             <td>
